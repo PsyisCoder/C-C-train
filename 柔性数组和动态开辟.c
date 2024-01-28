33326816374d4b83a7ec02e4e983e1d16769a27b
PsyCode <2601793620@qq.com>
2024-01-28 10:43:56 +0800

    柔性数组和动态开辟.c

---
+++ 柔性数组和动态开辟.c
@@ -0,0 +1,52 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+//struct S
+//{
+//	int n;
+//	int arr[0];
+//};
+struct S
+{
+	int n;
+	int* arr;
+};
+int main()
+{
+	/*struct S* p = (struct S*)malloc(sizeof(struct S) + 10 * sizeof(int));
+	p->n = 100;
+	for(int i=0;i<10;++i)
+	{
+		p->arr[i]=i;
+	}
+	for (int i = 0; i < 10; ++i)
+	{
+		printf("%d ", p->arr[i]);
+	}
+	free(p);
+	p = NULL;*/
+	struct S* p = (struct S*)malloc(sizeof(struct S));
+	p->arr = (int*)malloc(10 * sizeof(int));
+	p->n = 100;
+	for (int i = 0; i < 10; ++i)
+	{
+		p->arr[i] = i;
+	}
+	for (int i = 0; i < 10; ++i)
+	{
+		printf("%d ", p->arr[i]);
+	}
+	printf("\n");
+	int* s = (int*)realloc(p->arr, 20 * sizeof(int));
+	for (int i = 0; i < 20; ++i)
+	{
+		p->arr[i] = i;
+	}
+	for (int i = 0; i < 20; ++i)
+	{
+		printf("%d ", p->arr[i]);
+	}
+	free(p);
+	return 0;
+}
